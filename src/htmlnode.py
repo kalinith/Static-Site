6c5137b85e7baf8aa95416505591433941ed74e8
kalinith <kalinith@gmail.com.com>
2024-09-06 15:18:13 +0000

    the parents of the leaf now exist

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -36,7 +36,7 @@ class LeafNode(HTMLNode):
     def to_html(self):
         # for now I'm assuming if there is no tag there will be no props
         if self.value == None:
-            raise ValueError
+            raise ValueError("there is no value for this leaf")
         else:
             body = f'{self.value}'
 
@@ -50,3 +50,21 @@ class LeafNode(HTMLNode):
             attr = str(self.props_to_html())+">"
 
         return pre+attr+body+post
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        childrenstring = ""
+        if self.tag == None:
+            raise ValueError("this parent has no tag")
+        if self.children == None or self.children == []:
+            raise valueError("this Parent has no Children")
+        pre = f"<{self.tag}"
+        post = f"</{self.tag}>"
+        attr = str(self.props_to_html())+">"
+        for child in self.children:
+            childrenstring += child.to_html()
+        outputstring = pre+attr+childrenstring+post
+        return outputstring
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -8,22 +8,52 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode(tag="a", props={"href": "https://www.google.com", "target": "_blank",})
         node2 = HTMLNode("a",None,None,{"href": "https://www.google.com", "target": "_blank",})
         node3 = HTMLNode("h1","Heading")
-        node4 = HTMLNode("p","This is a paragraph")
+        node4 = LeafNode(tag="p",value="This is a paragraph")
 
         self.assertEqual(node, node2)
+        nodetest = node4.to_html()
+        self.assertEqual(nodetest, "<p>This is a paragraph</p>")
 
     def test_true(self):
         node5 = LeafNode("This is a Leafnode", "p")
         node6 = LeafNode("This is a paragraph of text.", tag="p")
         node7 = LeafNode("Click me!", "a", {"href": "https://www.google.com"})
         node8 = LeafNode("this is plain text")
-
-        print(node5)
-        print(f"Node5 should print out as <p>This is a Leafnode</p> : {node5.to_html()}")
+        node9 = ParentNode(
+            "p",
+            [
+                LeafNode("Bold text", "b"),
+                LeafNode("Normal text", None),
+                LeafNode("italic text", "i"),
+                LeafNode("Normal text", None),
+            ],
+        )
+        node10 = ParentNode(
+            tag="p",
+            children=
+                [
+                    LeafNode(tag="b", value="Bold text"),
+                    LeafNode(tag=None, value="Normal text"),
+                    LeafNode(tag="i", value="italic text"),
+                    LeafNode(tag=None, value="Normal text"),
+                ]
+        )
+        node11 = ParentNode(
+            "p",
+                [
+                    ParentNode(
+                        "i",
+                        [
+                            LeafNode(tag="b", value="Boldly Going"),
+                        ],
+                    ),
+                    LeafNode(tag=None, value="nowhere"),
+                    LeafNode(tag="i", value="slowly"),
+                 ],
+            )
         self.assertTrue(node5.to_html() == "<p>This is a Leafnode</p>")
-        print(f"Node6 should print out as <p>This is a paragraph of text. : {node6.to_html()}")
         self.assertTrue(node6.to_html() == f"<p>This is a paragraph of text.</p>")
-        print(f'Node7 should print out as <a href="https://www.google.com">Click me!</a> : {node7.to_html()}')
         self.assertTrue(node7.to_html() == f'<a href="https://www.google.com">Click me!</a>')
-        print(f"Node8 should print out as this is plain text : {node8.to_html()}")
         self.assertTrue(node8.to_html() == f"this is plain text")
+        self.assertTrue(node9.to_html() == node10.to_html())
+        self.assertTrue(node11.to_html() == "<p><i><b>Boldly Going</b></i>nowhere<i>slowly</i></p>")
