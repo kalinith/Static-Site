936dcf1854e1cadd6c84d45fa614e995b414220b
kalinith <kalinith@gmail.com.com>
2025-03-06 16:33:14 +0000

    path is now passable via cli parameter

--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
 from textnode import *
 from htmlnode import *
+import sys
+
 from file_ops import populate_directory, generate_pages_recursive
 
 def main():
     content_directory = 'content'
     static_files_directory = 'static'
-    destination_directory = "public"
+    destination_directory = "/"
     template = 'template.html'
 
-    node = TextNode("leroy Jenkins", "link","https://192.168.61.129:888")
-    print(node)
-    html = text_node_to_html_node(node)
-    print(html.to_html())
+    if len(sys.argv) == 2:
+        destination_directory = sys.argv[1]
 
     populate_directory(static_files_directory, destination_directory)
     generate_pages_recursive(content_directory, template, destination_directory)
